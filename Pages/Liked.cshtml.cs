36d873918c48b9f60c2d66e61d103b282644a21c
SaYMooN <shokoladnysaymoon@mail.ru>
2023-06-10 12:03:05 +0300

    Now the links are working as they should work, new pages have been added, EntityFramework is added in nugget packages

---
+++ Pages/Liked.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RandaManga.Pages.Shared
+{
+    public class LikedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RandaManga.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
