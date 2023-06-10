3099c4f70ac68d5c2c378f949207e5c39bd367d2
SaYMooN <shokoladnysaymoon@mail.ru>
2023-06-10 13:27:17 +0300

    manga class has been formed

---
+++ Models/Manga.cs
@@ -0,0 +1,15 @@
+﻿namespace RandaManga.Models
+{
+    class Manga
+    {
+        public string Name { get; private set; } = "Default_Manga_Name";
+        public string Description { get; private set; } = "Default_Manga_Description";
+        public MangaType Type { get; private set; }= MangaType.Unknown;
+        public MangaStatus Status { get; private set; }= MangaStatus.Unknown;
+        public string[] Tags { get; private set; }=new string[1] { "No tags were found" };
+        public string Link { get; private set; } = "No_Link_Was_Found";
+        public string ImagePath { get; private set; } = @"~/images/mangaCovers/Default.png";
+    }
+    enum MangaType {Manga,Manhwa, Manhua, Unknown };
+    enum MangaStatus { Ongoing, Announcement, Completed, Suspended, Unknown };
+}
