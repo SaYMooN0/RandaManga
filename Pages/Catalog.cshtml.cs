59e8d68e824cffc9becda627740e6731c179dc69
SaYMooN <shokoladnysaymoon@mail.ru>
2023-06-12 13:59:56 +0300

    Started to form catalog html and css

--- Pages/Catalog.cshtml.cs
+++ Pages/Catalog.cshtml.cs
@@ -21,9 +21,13 @@ namespace RandaManga.Pages
                 " когда оружие съест 99 злых душ, готовых стать Кишином, и 1 душу ведьмы, а потому все повелители активно соревнуются в создании" +
                 " Косы Смерти. Ну что, вы готовы окунуться в мир приключений?", MangaType.Manga, MangaStatus.Completed, "Боевик,Драма,Комедия,Приключения,Психология,Сверхъестественное,Сёнэн,Фэнтези,Этти,Зомби,Демоны,Магия,Боги,Бои на мечах,Борьба за власть,Волшебные существа,ГГ женщина,ГГ мужчина,Дружба,Жестокий мир,Злые духи,Магическая академия,Навыки / способности,Волшебники / маги",
                 "https://mangalib.org/soul_eater?section=info", "https://www.mangaread.org/manga/soul-eater/", @"/images/mangaCovers/souleater.jpg", "OHKUBO Atsushi", "16+", 2003);
-            context.MangaCatalog.Add(m);
-            context.SaveChanges();
-            mangaList = context.MangaCatalog.AsNoTracking().ToList();
+            //context.MangaCatalog.Add(m);
+            //context.SaveChanges();
+            //mangaList = context.MangaCatalog.AsNoTracking().ToList();
+            for(int i= 0;i<30;i++)
+            {
+                mangaList.Add(m);
+            }
         }
     }
 }
