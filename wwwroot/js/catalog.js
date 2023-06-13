b17287e4c5c9cc3572bcf6a15f80a76fe2d4d57e
SaYMooN <shokoladnysaymoon@mail.ru>
2023-06-14 02:58:04 +0300

    keep on working on catalog page

---
+++ wwwroot/js/catalog.js
@@ -0,0 +1,17 @@
+function tagsOnClickHandlerShow() {
+    var catalogFilterTagDiv = document.querySelector(".catalogFilterTagDiv");
+    var tagsChoosingMode = document.querySelector(".tagsChoosingMode");
+
+    catalogFilterTagDiv.style.display = "none";
+    tagsChoosingMode.style.display = "block";
+}
+function tagsOnClickHandlerHide() {
+    var catalogFilterTagDiv = document.querySelector(".catalogFilterTagDiv");
+    var tagsChoosingMode = document.querySelector(".tagsChoosingMode");
+
+    catalogFilterTagDiv.style.display = "grid";
+    tagsChoosingMode.style.display = "none";
+}
+function deleteAllTags() {
+    alert("all tags deleted");
+}
