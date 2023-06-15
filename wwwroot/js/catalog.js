81656ebbb8a5439510bbfe92b04004152b67272e
SaYMooN <shokoladnysaymoon@mail.ru>
2023-06-15 21:25:42 +0300

    keep on working on filters in catalog page

--- wwwroot/js/catalog.js
+++ wwwroot/js/catalog.js
@@ -1,16 +1,20 @@
 function tagsOnClickHandlerShow() {
+    var OtherFilters = document.querySelector(".allInfoInputForm");
+    OtherFilters.style.display = "none";
     var catalogFilterTagDiv = document.querySelector(".catalogFilterTagDiv");
     var tagsChoosingMode = document.querySelector(".tagsChoosingMode");
-
     catalogFilterTagDiv.style.display = "none";
     tagsChoosingMode.style.display = "block";
+
 }
 function tagsOnClickHandlerHide() {
-    var catalogFilterTagDiv = document.querySelector(".catalogFilterTagDiv");
+    var OtherFilters = document.querySelector(".allInfoInputForm");
+    OtherFilters.style.display = "block";
+    var catalogFilterTagDiv = document.getElementById("catalogFilterTagDiv");
     var tagsChoosingMode = document.querySelector(".tagsChoosingMode");
-
     catalogFilterTagDiv.style.display = "grid";
     tagsChoosingMode.style.display = "none";
+    console.log(1);
 }
 function deleteAllTags() {
     alert("all tags deleted");
