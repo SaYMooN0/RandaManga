77c79987b17beccfbeccfc75ba1b2dbc82ea4ed3
SaYMooN <shokoladnysaymoon@mail.ru>
2023-06-17 16:31:03 +0300

    Started to work on recomendation area on the main page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -7,9 +7,16 @@ namespace RandaManga.Pages
 {
     public class IndexModel : PageModel
     {
+        public Manga recomendstionRomance { get; private set; }
+        public ApplicationContext context;
+        public IndexModel(ApplicationContext db)
+        {
+            this.context = db;
+        }
         public void OnGet()
         {
-            
+            List<Manga> allMangaList = context.MangaCatalog.AsNoTracking().ToList();
+            recomendstionRomance = allMangaList.Find(m => m.Name == "ТораДора!");
         }
     }
 }
